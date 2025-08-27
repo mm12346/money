030628b11d6081cf9ec55f76508907630ca5adca
mm12346 <xoctell338@gmail.com>
2025-08-27 15:37:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wallet-wise-cache-v1.0.0';
+const CACHE_NAME = 'wallet-wise-cache-v1.0.1';
 const urlsToCache = [
   '/',
   'index.html', // สมมติว่าไฟล์หลักของคุณชื่อ index.html
